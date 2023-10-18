be102d4354eb1893fcc6f26e8c4145a0fb7e075c
Mark Skelton <mdskelton99@gmail.com>
2023-10-17 21:02:51 -0500

    Support passing id to edit

--- pkg/cmd/edit/edit.go
+++ pkg/cmd/edit/edit.go
@@ -17,28 +17,44 @@ var EditCmd = &cobra.Command{
     Edits a byte given a byte id or slug. If no id or slug is provided, a fuzzy
     search will be performed to find the byte to edit.
   `),
+	Args: cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		bytes, err := storage.SearchBytes(nil)
+		hasArg := len(args) > 0
+		bytes, err := storage.SearchBytes(func(byte storage.Byte) bool {
+			// If an arg is provided, filter by id or slug
+			if hasArg {
+				return byte.Id == args[0] || byte.Slug == args[0]
+			}
+
+			return true
+		})
 		if err != nil {
 			return err
 		}
 
 		if len(bytes) == 0 {
-			fmt.Println("no bytes found")
+			fmt.Println("no byte found")
 			return nil
 		}
 
-		var slugs []string
-		for _, item := range bytes {
-			slugs = append(slugs, item.Slug)
-		}
+		var slug string
 
-		slug, err := utils.Filter(slugs)
-		if err != nil {
-			return err
-		}
-		if slug == "" {
-			fmt.Println("no bytes found")
+		// Auto-select the byte if there is only one
+		if len(bytes) == 1 {
+			slug = bytes[0].Slug
+		} else {
+			var slugs []string
+			for _, item := range bytes {
+				slugs = append(slugs, item.Slug)
+			}
+
+			slug, err := utils.Filter(slugs)
+			if err != nil {
+				return err
+			}
+			if slug == "" {
+				fmt.Println("no bytes found")
+			}
 		}
 
 		var byte storage.Byte
